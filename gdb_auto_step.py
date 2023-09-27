d12490272b5a1adb8aa00909acdea7ac1070dda3
PiotrG <pitgal@gmail.com>
2023-09-27 22:42:58 +0200

    GDB konfiguracje

--- gdb_auto_step.py
+++ gdb_auto_step.py
@@ -6,7 +6,7 @@ def auto_step(event):
 	gdb.execute("x/i $eip")
 	gdb.execute("echo stack\\n")
 	gdb.execute("i r ebp\ni r esp")
-	gdb.execute("x/8x $sp")
-	gdb.execute("echo Help: i r -> rejestry, clear nazwabreakpointa\\n")
+	gdb.execute("x/8x $esp")
+	gdb.execute("echo Help: 'i r' -> rejestry, 'c brkpt' -> clear breakpoint, 'i b' -> info beakpt(lista) 'disas /r $eip, +6' \\n")
 
 gdb.events.stop.connect(auto_step)
